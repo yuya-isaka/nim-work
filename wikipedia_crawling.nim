7041fcb6eefc96ed49c53cdd9c23fc49c1751cf0
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-04-06 12:53:41 +0900

    add wikipedia crawling but recognitionfailed

--- wikipedia_crawling.nim
+++ wikipedia_crawling.nim
@@ -41,16 +41,24 @@ var elements = response.body.findAll(re"<img(.*?)>")
   Wikipediaの投稿画像は//upload.wikimedia.org/~という形式でimgタグに設定さあれている
   画像にアクセスするためにURLを組み立てる
   画像をローカルに保存するときファイル名はそのままにする
+  画像URLを/で区切り配列にして，配列の最後の要素をファイル名として取得
 ]#
 
+#[
+  reモジュールでエスケープ文字が使えない？
+  vscodeのNimでの"""の認識がおかしい
+]#
 for element in elements:
-  var range = element.findBounds(re"""
-    src=\"(.*?)\"
-    """)
+  var range = element.findBounds(re"""src=\"(.*?)\"""")
+  # おかしい
+
   var attr = element[range.first..range.last]
   var img = attr[5..^2]
 
   if img[0..7] == "//upload":
     img = "https:" & img
+    var splited = img.split(re"/")
+    var fileName = splited[high(splited)]
+    client.downloadFile(img, fileName)
 
 
